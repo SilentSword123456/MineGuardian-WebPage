f82510c664bab7e61f98cb77f987d479af0ad129
SilentSword <andrei925.dumitru@gmail.com>
2025-12-23 20:08:09 +0200

    Simplify connection log messages in webSocket.js

--- src/utils/webSocket.js
+++ src/utils/webSocket.js
@@ -3,11 +3,11 @@ import { io } from 'socket.io-client';
 const socket = io('http://localhost:5000');
 
 socket.on('connect', () => {
-    console.log('✅ Connected to server');
+    console.log('Connected to server');
 });
 
 socket.on('disconnect', () => {
-    console.log('❌ Disconnected');
+    console.log('Disconnected from server');
 });
 
 export default socket;
