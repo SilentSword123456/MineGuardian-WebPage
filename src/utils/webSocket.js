912cdacc0549fc4f8c34e9d48c5577a6e7d2e721
SilentSword <andrei925.dumitru@gmail.com>
2025-12-23 21:18:14 +0200

    Refactor webSocket.js to create a socket instance with serverId as a query parameter

--- src/utils/webSocket.js
+++ src/utils/webSocket.js
@@ -1,13 +1,18 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const createSocket = (serverId) => {
+    const socket = io('http://localhost:5000', {
+        query: { serverId: serverId }
+    });
 
-socket.on('connect', () => {
-    console.log('Connected to server');
-});
+    socket.on('connect', () => {
+        console.log('Connected to server');
+    });
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-});
+    socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+    });
 
-export default socket;
+    return socket;
+}
+export default createSocket;
