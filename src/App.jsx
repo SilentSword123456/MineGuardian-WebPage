e678721c5a006db61552df79e6de1cf1476eda51
SilentSword <andrei925.dumitru@gmail.com>
2025-12-23 19:00:16 +0200

    Integrate ServerPage component and set up React Query for server management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import ServersBar from "./components/ServersBar.jsx";
+import ServerPage from "./components/ServerPage.jsx";
+import {useState} from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+
+const queryClient = new QueryClient();
+
+/**
+ * @typedef {import('../types/server.js').Server} Server
+ */
 function App() {
 
+    /** @type {Server} */
+    const server = [{id: null, name: null}];
+  const [loadedServer, setLoadedServer] = useState(server);
+
   return (
-      <ServersBar/>
+        <QueryClientProvider client={queryClient}>
+          <div style={{ display: 'flex' }}>
+            <ServersBar loadServer={setLoadedServer}/>
+            <ServerPage loadedServer={loadedServer}/>
+          </div>
+      </QueryClientProvider>
+
   )
 }
 
