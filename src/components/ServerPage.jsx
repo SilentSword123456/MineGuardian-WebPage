1499ef938d77fe3e06815ac54f304bfa3d5b45f3
Dumitru Andrei <andrei925.dumitru@gmail.com>
2025-12-23 20:00:20 +0200

    Add Console component to ServerPage for enhanced server interaction

--- src/components/ServerPage.jsx
+++ src/components/ServerPage.jsx
@@ -1,15 +1,19 @@
+import Console from "./Console.jsx";
+
 /**
  * @typedef {import('../types/server.js').Server} Server
  */
-
 /**
  * @param {Object} props
  * @param {Server} props.loadedServer
  */
 function ServerPage({loadedServer}) {
+
+
     return (
         <div>
             Current loaded server: {loadedServer.name} with id {loadedServer.id}
+            <Console/>
         </div>
     )
 }
