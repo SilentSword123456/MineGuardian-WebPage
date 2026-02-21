3070ecccb569feb149bbdd973e22b46d526c1b21
SilentSword <andrei925.dumitru@gmail.com>
2026-02-21 14:56:25 +0200

    Enhance QuickCommands component with improved button UI and layout for server controls

--- src/components/QuickCommands.jsx
+++ src/components/QuickCommands.jsx
@@ -1,4 +1,6 @@
-import Server from "../types/Server";
+import Server from "../types/server.jsx";
+import Button from "./ui/Button.jsx";
+import {Play, Square} from "lucide-react";
 
 /**
  * @param {Object} props
@@ -9,16 +11,24 @@ function QuickCommands({server}) {
     return (
         <div className="quick-commands">
             <h3>Quick Commands</h3>
-            <button
-                onClick={() => server.start()}
-                disabled={server.isRunning}>
-                start
-            </button>
-            <button
-                onClick={() => server.stop()}
-                disabled={!server.isRunning}>
-                stop
-            </button>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                <Button
+                    onClick={() => server.start()}
+                    disabled={server.isRunning}
+                    icon={Play}
+                    variant="primary"
+                >
+                    Start Server
+                </Button>
+                <Button
+                    onClick={() => server.stop()}
+                    disabled={!server.isRunning}
+                    icon={Square}
+                    variant="danger"
+                >
+                    Stop Server
+                </Button>
+            </div>
         </div>
     )
 }
