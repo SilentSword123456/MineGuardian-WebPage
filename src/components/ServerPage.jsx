d053a4a9ee4c8bae323770afb44da47aa7f4bb69
SilentSword <andrei925.dumitru@gmail.com>
2026-02-22 20:29:56 +0200

    Implement socket management in ServerPage component to handle connection and message events

--- src/components/ServerPage.jsx
+++ src/components/ServerPage.jsx
@@ -3,6 +3,7 @@ import Console from "./Console.jsx";
 import QuickCommands from "./QuickCommands.jsx";
 import PlayerAvatar from "./PlayerAvatar.jsx";
 import ServerStats from "./ServerStats.jsx";
+import createSocket from "../utils/webSocket.js";
 
 /**
  * @typedef {import('../types/server.jsx').Server} Server
@@ -13,9 +14,53 @@ import ServerStats from "./ServerStats.jsx";
  */
 function ServerPage({loadedServer}) {
     const [data, setData] = useState(null);
+    const [socket, setSocket] = useState(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         setData(null);
+        setMessages([]);
+
+        if(!loadedServer || !loadedServer.name)
+            return;
+
+        console.log(`Creating socket for server: ${loadedServer.name}`);
+
+        const newSocket = createSocket(loadedServer.name)
+        setSocket(newSocket);
+
+        newSocket.on('connect', () => {
+            setIsConnected(true);
+        });
+
+        newSocket.on('disconnect', () => {
+            setIsConnected(false);
+        });
+
+        newSocket.on('message', (data) => {
+            setMessages(prev => [...prev, {type:"", text: data.data }]);
+        });
+
+        newSocket.on('console', (data) => {
+            setMessages(prev => [...prev, {type:"", text: data.data }]);
+        });
+
+        newSocket.on('stats', (data) => {
+            console.log('Received stats from socket:', data);
+            setData(data);
+        });
+
+        return () => {
+            newSocket.off('connect');
+            newSocket.off('disconnect');
+            newSocket.off('message');
+            newSocket.off('console');
+            newSocket.off('stats');
+            newSocket.disconnect();
+            setSocket(null);
+            setIsConnected(false);
+        };
     }, [loadedServer.name]);
 
     return (
@@ -32,7 +77,13 @@ function ServerPage({loadedServer}) {
                             memoryUsageMb={data?.memory_usage_mb}
                         />
                     </div>
-                    <Console server={loadedServer} onStats={setData} />
+                    <Console
+                        server={loadedServer}
+                        socket={socket}
+                        isConnected={isConnected}
+                        messages={messages}
+                        setMessages={setMessages}
+                    />
                     <QuickCommands server={loadedServer} />
                 </>
             )}
