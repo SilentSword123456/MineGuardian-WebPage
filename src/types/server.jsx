eeae29dd42251960a81386a1c761f1f96393f3b7
SilentSword <andrei925.dumitru@gmail.com>
2026-02-22 20:15:59 +0200

    Remove unused getStatus and getOnlinePlayers methods from server component

--- src/types/server.jsx
+++ src/types/server.jsx
@@ -43,27 +43,7 @@ class Server {
         }
     }
 
-    getStatus() {
-        return this.isRunning ? 'Online' : 'Offline';
-    }
 
-    async getOnlinePlayers() {
-        try {
-            const response = await fetch(`${this.baseUrl}/servers/${this.name}`, {
-                method: 'GET'
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const result = await response.json();
-            return result["online_players"]["online"];
-        } catch (error) {
-            console.error(`Error stopping server ${this.name}:`, error);
-            throw error;
-        }
-    }
 
 }
 
