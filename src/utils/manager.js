2ddf13b9d468d0d70c4af9e14a0253b2cde949b6
SilentSword <andrei925.dumitru@gmail.com>
2026-03-01 13:21:46 +0200

    Add method to fetch available software versions from the server

--- src/utils/manager.js
+++ src/utils/manager.js
@@ -15,6 +15,23 @@ class Manager{
             throw error;
         }
     }
+
+    async getAvailableVersions(Software = "Vanilla") {
+        try {
+            const response = await fetch(`${this.baseUrl}/manage/${Software}/getAvailableVersions`, {
+                method: 'GET'
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.error(`Error fetching versions for ${Software}:`, error);
+            throw error;
+        }
+    }
 }
 
 export default new Manager();
