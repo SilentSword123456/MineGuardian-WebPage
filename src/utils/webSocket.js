6002f00ebc70b99681bd85de83f43ac67bf9da9a
SilentSword <andrei925.dumitru@gmail.com>
2026-03-03 22:05:47 +0200

    Update WebSocket connection to use BASE_URL from configuration

--- src/utils/webSocket.js
+++ src/utils/webSocket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
+import { BASE_URL } from "@/lib/config.js";
 
 const createSocket = (serverName) => {
-    const socket = io('http://localhost:5000', {
+    const socket = io(BASE_URL, {
         query: { serverName: serverName },
         transports: ['websocket']
     });
