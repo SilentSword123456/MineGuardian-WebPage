7a99947aa5153dfec7d58baad8e564eb6af04ca5
SilentSword <andrei925.dumitru@gmail.com>
2026-03-26 22:07:38 +0200

    Add custom hook for global resource management with automatic refetching

---
+++ src/hooks/use-global-resources.jsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { useBackend } from "@/context/BackendContext.jsx";
+import manager from "@/utils/manager.js";
+
+export const DEFAULT_GLOBAL_RESOURCES = {
+    cpu_usage_percent: 0,
+    memory_usage_mb: 0,
+    online_players_count: 0,
+    max_memory_mb: 1,
+};
+
+export function useGlobalResources() {
+    const { backendUp } = useBackend();
+
+    const query = useQuery({
+        queryKey: ["global-resources"],
+        queryFn: () => manager.getGlobalUsedResources(),
+        enabled: backendUp === true,
+        refetchInterval: 10 * 1000,
+    });
+
+    const displayedGlobalResources = backendUp
+        ? (query.data ?? DEFAULT_GLOBAL_RESOURCES)
+        : DEFAULT_GLOBAL_RESOURCES;
+
+    return {
+        ...query,
+        displayedGlobalResources,
+        refetchGlobalResources: query.refetch,
+    };
+}
+
