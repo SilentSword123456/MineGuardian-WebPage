b295e8d1504bdbe31d7289d83984b3838a052a7d
SilentSword <andrei925.dumitru@gmail.com>
2026-04-15 18:42:29 +0300

    Add Settings component import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import AppSidebar from "./components/AppSidebar.jsx";
 import { Navigate, Route, Routes, useNavigate, useParams } from "react-router-dom";
 import { useServers } from "@/hooks/use-servers.jsx";
 import PlayerManager from "@/components/PlayerManager.jsx";
+import Settings from "@/components/Settings.jsx";
 
 const queryClient = new QueryClient();
 
